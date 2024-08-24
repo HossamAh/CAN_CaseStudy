c5a07381b27439227d5c4b68cfb167a63a7723b0
Hossam Ahmed <hossamahmed201515@gmail.com>
2024-08-24 19:19:47 +0300

    interrupt mode is added

--- startup.c
+++ startup.c
@@ -1,31 +1,95 @@
 // startup.c 
-// Ahmed Elnabawe 
 #include "COTS/LIB/Std_Types.h"
-void Rest_Handler() ;
+void Reset_Handler() ;
 void Default_Handler(){
-	Rest_Handler();
+	Reset_Handler();
 }
 void NMI_Handler() __attribute__((weak,alias("Default_Handler")));
-void H_fault_Handler() __attribute__((weak,alias("Default_Handler")));
-void MM_Fault_Handler() __attribute__((weak,alias("Default_Handler")));
-void Bus_Fault() __attribute__((weak,alias("Default_Handler")));
-void Usage_Fault_Handler() __attribute__((weak,alias("Default_Handler")));
+void HardFault_Handler() __attribute__((weak,alias("Default_Handler")));
+void MemManage_Handler() __attribute__((weak,alias("Default_Handler")));
+void BusFault_Handler() __attribute__((weak,alias("Default_Handler")));
+void UsageFault_Handler() __attribute__((weak,alias("Default_Handler")));
+void USB_HP_CAN1_TX_IRQHandler() __attribute__((weak,alias("Default_Handler")));
+void USB_LP_CAN1_RX0_IRQHandler() __attribute__((weak,alias("Default_Handler")));
+void CAN1_RX1_IRQHandler() __attribute__((weak,alias("Default_Handler")));
+void CAN1_SCE_IRQHandler() __attribute__((weak,alias("Default_Handler")));
+void SysTick_Handler() __attribute__((weak,alias("Default_Handler")));
 extern uint32 _stak_top;
-uint32 vectors[] __attribute__((section(".vectors")))={
+uint32 vectors[70] __attribute__((section(".vectors")))={
 	(uint32) &_stak_top, 
-	(uint32) &Rest_Handler,
+	(uint32) &Reset_Handler,
 	(uint32) &NMI_Handler,
-	(uint32) &H_fault_Handler ,
-	(uint32) &MM_Fault_Handler,
-	(uint32) &Bus_Fault ,
-	(uint32) &Usage_Fault_Handler
+	(uint32) &HardFault_Handler ,
+	(uint32) &MemManage_Handler,
+	(uint32) &BusFault_Handler ,
+	(uint32) &UsageFault_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& SysTick_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& USB_HP_CAN1_TX_IRQHandler
+   ,(uint32)& USB_LP_CAN1_RX0_IRQHandler
+   ,(uint32)& CAN1_RX1_IRQHandler
+   ,(uint32)& CAN1_SCE_IRQHandler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
+   ,(uint32)& Default_Handler
 };
+
 extern uint32 _E_text;
 extern uint32 _S_data;
 extern uint32 _E_data;
 extern uint32 _S_bss;
 extern uint32 _E_bss;
-void Rest_Handler()
+void Reset_Handler()
 {
 	// copy .data from flash to sram
 	int i ;
